e93bc78955f166ff36e1f62a837bc1cfd39e4894
Kevin Lee <4321872+akevinlee@users.noreply.github.com>
2020-07-12 14:57:12 +0100

    Updated fortify-sca.bat

--- fortify-wi.bat
+++ fortify-wi.bat
@@ -22,7 +22,7 @@ call "C:\Micro Focus\Fortify WebInspect\WISwag.exe" -i ".\etc\WiSwagConfig.json"
 echo ************************************************************
 echo Executing dynamic scan...
 echo ************************************************************
-call "C:\Micro Focus\Fortify WebInspect\WI.exe" -s ".\etc\DefaultSettings.xml" -macro ".\etc\Login.webmacro" -u "https://localhost:6443/secure-web-app/" -ep ".\target\wi-secure-web-app.fpr" -ps 1008
+call "C:\Micro Focus\Fortify WebInspect\WI.exe" -s ".\etc\DefaultSettings.xml" -macro ".\etc\Login.webmacro" -u "https://localhost:6443/iwa/" -ep ".\target\wi-iwa.fpr" -ps 1008
 
 :: Stop the application
 echo ************************************************************
@@ -33,5 +33,5 @@ call mvn -Pwlp liberty:stop
 echo ************************************************************
 echo Generating report
 echo ************************************************************
-call ReportGenerator -user "Demo User" -format pdf -f .\target\wi-secure-web-app.pdf -source .\target\wi-secure-web-app.fpr
+call ReportGenerator -user "Demo User" -format pdf -f .\target\wi-iwa.pdf -source .\target\iwa.fpr
 
