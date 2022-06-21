69829c328e68d21f0d1ff7dcf24d33639ce2ab3a
kadraman <kevin.lee@microfocus.com>
2022-06-21 09:57:49 +0100

    Refactoring for iwa.onfortify.com

--- bin/build-container.sh
+++ bin/build-container.sh
@@ -3,9 +3,8 @@ cd /usr/share/repos/IWAPharmacyDirect
 docker-compose down
 git pull
 mvn clean package
-ln -s ./src/main/configs/docker-compose.yml .
-docker build -f src/main/configs/Dockerfile -t iwa:latest .
+docker build -f Dockerfile -t iwa:latest .
 docker login
-docker tag iwa:latest mfdemouk/iwa:latest
-docker push mfdemouk/iwa:latest
+#docker tag iwa:latest mfdemouk/iwa:latest
+#docker push mfdemouk/iwa:latest
 docker-compose up -d
