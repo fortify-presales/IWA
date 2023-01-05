bdd035522d2eeb26c2bcf2f875659159d5dbbead
kadraman <kevin.lee@microfocus.com>
2023-01-05 12:23:44 +0000

    Updating GitHub Actions

--- bin/redeploy.sh
+++ bin/redeploy.sh
@@ -1,18 +1,20 @@
-#!/bin/sh
+#!/bin/bash
 
 # Sample script to build and redeploy application as docker image
 
-commit_id=$1
-commiter_name=$2
-commiter_email=$3
+#commit_id=$1
+#commiter_name=$2
+#commiter_email=$3
 
-echo "Redeploying commit id $commit_id from $commiter_name ..."
+#echo "Redeploying commit id $commit_id from $commiter_name ..."
+echo "Redeploying application ..."
 
-#cd /usr/share/repos/IWAPharmacyDirect
+pushd /usr/share/repos/IWAPharmacyDirect
 git pull --force
-#docker-compose down
+docker-compose down
 mvn clean package
-#docker build . --tag iwa:latest
-#docker-compose up -d
+docker build . --tag iwa:latest
+docker-compose up -d
+popd
 
 echo "Done."
