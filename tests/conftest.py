ed743b4cfc7380361128ba278b087d90ca0321c0
kadraman <kevin.lee@microfocus.com>
2024-06-12 18:37:32 +0100

    Updating FAST Scripts

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,6 +7,7 @@ from webdriver_manager.chrome import ChromeDriverManager
 def chrome_browser():
     #driver = webdriver.Chrome()
 
+    FAST_PROXY="35.176.81.197:8087"
 
     options = webdriver.ChromeOptions()
     options.add_argument("--start-maximized")
@@ -15,7 +16,7 @@ def chrome_browser():
     options.add_argument("--incognito")
     options.add_argument("--headless=new")
     # comment out below if not using FAST proxy
-    #chrome_options.add_argument('--proxy-server=%s' % FAST_PROXY)
+    options.add_argument('--proxy-server=http://35.176.81.197:8087')
 
     # Use this line instead of the prev if you wish to download the ChromeDriver binary on the fly
     driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()),
